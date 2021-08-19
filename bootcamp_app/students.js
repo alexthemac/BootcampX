45f191c9a7e0d9657187f2894c0f9f3d239dd081
Alex MacLure <almaclure@gmail.com>
2021-08-19 02:31:38 +0000

    Update queries to prevent SQL injection

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -1,3 +1,4 @@
+//Initial setup of postgreSQL and node
 const { Pool } = require('pg');
 
 const pool = new Pool({
@@ -7,15 +8,26 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-// console.log(process.argv[2],process.argv[3]);
+//Log connected once connection made
+console.log("connected");
 
-pool.query(`
-SELECT students.id as student_id, students.name as name, cohorts.name as cohort
-FROM students
-JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name LIKE '%${process.argv[2]}%'
-LIMIT ${process.argv[3] || 5};
-`)
+//Query that will use command line arguments. This is to prevent SQL injection attack ($1 is the first value in the values array, $2 is second etc...)
+const queryString = `
+  SELECT students.id as student_id, students.name as name, cohorts.name as cohort
+  FROM students
+  JOIN cohorts ON cohorts.id = cohort_id
+  WHERE cohorts.name LIKE $1
+  LIMIT $2;
+  `;
+
+//Values that come from command line arguments. This is to prevent SQL injection attack
+const cohortName = process.argv[2];
+const limit = process.argv[3] || 5;
+
+const values = [`%${cohortName}%`, limit];
+
+//Do the query
+pool.query(queryString, values)
 .then(res => {
   res.rows.forEach(user => {
     console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -1,3 +1,4 @@
+//Initial setup of postgreSQL and node
 const { Pool } = require('pg');
 
 const pool = new Pool({
@@ -7,17 +8,27 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
+//Log connected once connection made
 console.log("connected");
 
-pool.query(`
+//Query that will use command line arguments. This is to prevent SQL injection attack ($1 is the first value in the values array, $2 is second etc...)
+const queryString = `
 SELECT DISTINCT teachers.name AS teacher, cohorts.name as cohort
 FROM assistance_requests
 JOIN students ON students.id = student_id
 JOIN cohorts ON cohorts.id = cohort_id
 JOIN teachers ON teachers.id = teacher_id
-WHERE cohorts.name LIKE '${process.argv[2] || 'JUL02'}'
+WHERE cohorts.name LIKE $1
 ORDER BY teacher;
-`)
+`;
+
+//Values that come from command line arguments. This is to prevent SQL injection attack
+const like = process.argv[2] || 'JUL02';
+
+const values = [`%${like}%`];
+
+//Do the query
+pool.query(queryString, values)
 .then(res => {
   res.rows.forEach(row => {
     console.log(`${row.cohort}: ${row.teacher}`);
